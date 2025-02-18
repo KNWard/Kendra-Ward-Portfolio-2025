a11154695720e3a3a13790794e75a10ea78debd6
Kendra <92537842+KNWard@users.noreply.github.com>
2025-02-18 10:48:17 -0500

    Removed dark/light mode code from JS. Will work on at later date. Remove dark/light mode buttons from HTML. Removed commented lines from CSS.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// DARK/LIGHT MODE
-document.getElementById('lightMode').addEventListener('click', lightScreenMode);
-document.getElementById('darkMode').addEventListener('click', darkScreenMode);
-
-function lightScreenMode() {
-  document.getElementById('header').style.background = '#f4e7f9';
-}
-
-
-function darkScreenMode() {
-  document.getElementById('header').style.backgroundColor = '#17181d';
-}
-
-
 // OPEN BURGER MENU
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".navbar");
